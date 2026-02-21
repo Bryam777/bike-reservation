1be6a32f8334f531fc1babf0594958361820dd48
Bryam777 <bryam7777777@gmail.com>
2026-02-21 04:35:47 -0500

    Poniendo parametro con promesa reutilizable, para las mismas funciones

--- src/app/api/bicicletas/[id]/route.ts
+++ src/app/api/bicicletas/[id]/route.ts
@@ -1,72 +1,74 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { BicicletaServicio }         from '@/services/bike.service'
 
+type Params = { params: Promise<{id: string}>}
+
 // Retorna una bicicleta por su id
 export async function GET(
   _request: NextRequest,
-  { params }: { params: Promise<{ id: string }> }
+  { params }: Params
 ) {
   try {
-    const {id} = await params
+    const { id } = await params
     const bicicleta = await BicicletaServicio.obtenerBicicletaPorId(Number(id))
-    console.log("SUPABASE URL:", process.env.NEXT_PUBLIC_SUPABASE_URL)
+
     return NextResponse.json({
-      data:    bicicleta,
-      error:   null,
+      data: bicicleta,
+      error: null,
       mensaje: 'Bicicleta obtenida correctamente',
     })
 
   } catch (error: any) {
     return NextResponse.json({
-      data:    null,
-      error:   error.message,
+      data: null,
+      error: error.message,
       mensaje: 'No se encontró la bicicleta',
     }, { status: 404 })
   }
 }
 
-// Actualiza una bicicleta existente
 export async function PUT(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: Params
 ) {
   try {
-    const body       = await request.json()
-    const bicicleta  = await BicicletaServicio.actualizarBicicleta(Number(params.id), body)
+    const { id } = await params
+    const body = await request.json()
+    const bicicleta = await BicicletaServicio.actualizarBicicleta(Number(id), body)
 
     return NextResponse.json({
-      data:    bicicleta,
-      error:   null,
+      data: bicicleta,
+      error: null,
       mensaje: 'Bicicleta actualizada correctamente',
     })
 
   } catch (error: any) {
     return NextResponse.json({
-      data:    null,
-      error:   error.message,
+      data: null,
+      error: error.message,
       mensaje: 'No se pudo actualizar la bicicleta',
     }, { status: 400 })
   }
 }
 
-// Elimina una bicicleta
 export async function DELETE(
   _request: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: Params
 ) {
   try {
-    await BicicletaServicio.eliminarBicicleta(Number(params.id))
+    const { id } = await params
+    await BicicletaServicio.eliminarBicicleta(Number(id))
 
     return NextResponse.json({
-      data:    null,
-      error:   null,
+      data: null,
+      error: null,
       mensaje: 'Bicicleta eliminada correctamente',
     })
 
   } catch (error: any) {
     return NextResponse.json({
-      data:    null,
-      error:   error.message,
+      data: null,
+      error: error.message,
       mensaje: 'No se pudo eliminar la bicicleta',
     }, { status: 400 })
   }
